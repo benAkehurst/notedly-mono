9441850b780cbdd947ecdb4adbd0bb0b3a8d4103
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-10 16:26:20 +0100

    desktop build

---
+++ desktop/src/index.js
@@ -0,0 +1,59 @@
+const { app, BrowserWindow } = require('electron');
+const { is, setContentSecurityPolicy } = require('electron-util');
+const config = require('./config');
+
+let window;
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  if (is.development) {
+    window.loadURL(config.LOCAL_WEB_URL);
+  } else {
+    window.loadURL(config.PRODUCTION_WEB_URL);
+  }
+
+  if (is.development) {
+    window.webContents.openDevTools();
+  }
+
+  if (!is.development) {
+    setContentSecurityPolicy(`
+    default-src 'none';
+    script-src 'self';
+    img-src 'self' https://www.gravatar.com;
+    style-src 'self' 'unsafe-inline';
+    font-src 'self';
+    connect-src 'self' ${config.PRODUCTION_API_URL};
+    base-uri 'none';
+    form-action 'none';
+    frame-ancestors 'none';
+  `);
+  }
+
+  // when the window is closed, dereference the window object
+  window.on('closed', () => {
+    window = null;
+  });
+}
+
+// when electron is ready, create the application window
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform === 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (window === null) {
+    createWindow();
+  }
+});
