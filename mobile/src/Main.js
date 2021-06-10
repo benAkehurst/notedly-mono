036531933fac0b4cc5639fe3062cf2a62cd759f1
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-10 16:35:43 +0100

    add routing and separate screens

--- mobile/src/Main.js
+++ mobile/src/Main.js
@@ -1,31 +1,8 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
-import styled from 'styled-components/native';
-
-const StyledView = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const H1 = styled.Text`
-  font-size: 48px;
-  font-weight: bold;
-`;
-
-const P = styled.Text`
-  margin: 24px 0;
-  font-size: 18px;
-`;
+import Screens from './screens';
 
 const Main = () => {
-  return (
-    <StyledView>
-      <H1>Hello World!</H1>
-      <P>This is my app</P>
-      <Image source={require('../assets/images/hello-world.jpg')}></Image>
-    </StyledView>
-  );
+  return <Screens />;
 };
 
 export default Main;
