af9b7ffa50df6a3ca1691f8975dabaf4d420e729
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-10 16:50:39 +0100

    add routing between screens and title in bar

--- mobile/src/screens/index.js
+++ mobile/src/screens/index.js
@@ -1,26 +1,44 @@
 import React from 'react';
+import { Text, View, ScrollView, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Feed from './feed';
 import Favorites from './favorites';
 import MyNotes from './mynotes';
+import NoteSceen from './note';
+
+const FeedStack = createStackNavigator({
+  Feed: Feed,
+  Note: NoteSceen,
+});
+
+const MyStack = createStackNavigator({
+  MyNotes: MyNotes,
+  Note: NoteSceen,
+});
+
+const FavStack = createStackNavigator({
+  Favorites: Favorites,
+  Note: NoteSceen,
+});
 
 const TabNavigator = createBottomTabNavigator({
   FeedScreen: {
-    screen: Feed,
+    screen: FeedStack,
     navigationOptions: {
       tabBarLabel: 'Feed',
     },
   },
   MyNoteScreen: {
-    screen: MyNotes,
+    screen: MyStack,
     navigationOptions: {
       tabBarLabel: 'My Notes',
     },
   },
   FavoritesScreen: {
-    screen: Favorites,
+    screen: FavStack,
     navigationOptions: {
       tabBarLabel: 'Favorites',
     },
