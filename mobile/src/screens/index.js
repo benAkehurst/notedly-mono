ec86640959766cad8f2eebdb93205dbbb19696d3
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-10 16:57:24 +0100

    add icons to menu

--- mobile/src/screens/index.js
+++ mobile/src/screens/index.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Feed from './feed';
 import Favorites from './favorites';
@@ -29,18 +30,27 @@ const TabNavigator = createBottomTabNavigator({
     screen: FeedStack,
     navigationOptions: {
       tabBarLabel: 'Feed',
+      tabBarIcon: ({ tintColor }) => (
+        <MaterialCommunityIcons name="home" size={24} color={tintColor} />
+      ),
     },
   },
   MyNoteScreen: {
     screen: MyStack,
     navigationOptions: {
       tabBarLabel: 'My Notes',
+      tabBarIcon: ({ tintColor }) => (
+        <MaterialCommunityIcons name="notebook" size={24} color={tintColor} />
+      ),
     },
   },
   FavoritesScreen: {
     screen: FavStack,
     navigationOptions: {
       tabBarLabel: 'Favorites',
+      tabBarIcon: ({ tintColor }) => (
+        <MaterialCommunityIcons name="star" size={24} color={tintColor} />
+      ),
     },
   },
 });
