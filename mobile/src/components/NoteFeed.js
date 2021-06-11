462af271892e261b11a41181910da1c6ef7753c1
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-11 14:23:46 +0100

    show notes on screen

--- mobile/src/components/NoteFeed.js
+++ mobile/src/components/NoteFeed.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 import Note from './Note';
@@ -16,30 +16,25 @@ const Separator = styled.View`
   background-color: #ced0ce;
 `;
 
-const notes = [
-  { id: 0, content: 'Bulk upload' },
-  { id: 1, content: 'Store Picker integration' },
-  { id: 2, content: 'Kg/item bug' },
-  { id: 3, content: 'Add to basket' },
-  { id: 4, content: 'Metrics' },
-  { id: 5, content: 'New book slot page' },
-  { id: 6, content: 'Automation' },
-  { id: 7, content: 'Perf: Slots' },
-  { id: 8, content: 'Display Cnc grid' },
-  { id: 9, content: 'approx prices' },
-];
-
 const NoteFeed = (props) => {
   return (
     <View>
       <FlatList
-        data={notes}
+        data={props.notes}
         keyExtractor={({ id }) => id.toString()}
         ItemSeparatorComponent={() => <Separator />}
         renderItem={({ item }) => (
-          <FeedView>
-            <Note note={item} />
-          </FeedView>
+          <TouchableOpacity
+            onPress={() => {
+              props.navigation.navigate('Note', {
+                id: item.id,
+              });
+            }}
+          >
+            <FeedView>
+              <Note note={item} />
+            </FeedView>
+          </TouchableOpacity>
         )}
       />
     </View>
