3449b0c917fc2e893e1cbb3e428d0c39d5b8c534
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-11 14:40:30 +0100

    Add loading component

--- mobile/src/screens/note.js
+++ mobile/src/screens/note.js
@@ -3,6 +3,7 @@ import { Text } from 'react-native';
 import { useQuery, gql } from '@apollo/client';
 
 import Note from '../components/Note';
+import Loading from '../components/Loading';
 
 const GET_NOTE = gql`
   query note($id: ID!) {
@@ -24,7 +25,7 @@ const NoteScreen = (props) => {
   const id = props.navigation.getParam('id');
   const { loading, error, data } = useQuery(GET_NOTE, { variables: { id } });
 
-  if (loading) return <Text>Loading...</Text>;
+  if (loading) return <Loading />;
   if (error) return <Text>Error getting note, {`${error}`}</Text>;
   return <Note note={data.note} />;
 };
