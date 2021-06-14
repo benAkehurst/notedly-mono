af2e16e6b04f46c628a97dd4761f648ea1db6294
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-06-14 15:23:23 +0100

    add signout button styling

--- mobile/src/screens/settings.js
+++ mobile/src/screens/settings.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { View, Button } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
+import styled from 'styled-components/native';
+
+const SignOutButton = styled.TouchableOpacity`
+  background: #0077cc;
+  width: 90%;
+  padding: 8px;
+  margin-top: 16px;
+`;
+
+const ButtonText = styled.Text`
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  font-size: 18px;
+`;
 
 const Settings = (props) => {
   const signOut = () => {
@@ -9,8 +24,10 @@ const Settings = (props) => {
     );
   };
   return (
-    <View>
-      <Button title={'Sign out'} onPress={signOut} />
+    <View style={{ display: 'flex', alignItems: 'center' }}>
+      <SignOutButton onPress={signOut}>
+        <ButtonText>Sign Out</ButtonText>
+      </SignOutButton>
     </View>
   );
 };
